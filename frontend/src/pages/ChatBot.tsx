e4c16e9c93396aeef1fea4bddf5cd56876f788d8
neha-peddinti <peddinti@seas.upenn.edu>
2025-04-13 21:21:33 +0000

    for css

--- frontend/src/pages/ChatBot.tsx
+++ frontend/src/pages/ChatBot.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import { useState } from 'react'
 import axios from 'axios';
-//import { useParams } from 'react-router-dom';
-//import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const MessageComponent = ({ sender, message }) => {
     return (
@@ -16,18 +15,11 @@ const MessageComponent = ({ sender, message }) => {
 
 export default function ChatBot() {
     console.log("in ChatBot()");
-    /*
+
     const [messages, setMessages] = useState([{ sender: 'chatbot', message: 'Hi there! What movie review questions do you have?' }]);
     const [input, setInput] = useState<string>('');
     const { username } = useParams();
     const navigate = useNavigate(); 
-
-    const feed = () => {
-        navigate('/' + username + '/home');
-    };
-    const friends = () => {
-        navigate("/"+ username+"/friends");
-    };
 
     const sendMessage = async () => {
         // CUT HERE 
@@ -46,21 +38,42 @@ export default function ChatBot() {
         }
         // END CUT
     }
-    */
 
     return (
         <div className='w-screen h-screen flex flex-col items-center'>
         <div className='w-full h-16 bg-slate-50 flex justify-center mb-2'>
             <div className='text-2xl max-w-[1800px] w-full flex items-center'>
-            Pennstagram&nbsp;
-            <button type="button" className='px-2 py-2 rounded-md bg-gray-500 outline-none text-white'
-              >Feed</button>&nbsp;
-            <button type="button" className='px-2 py-2 rounded-md bg-gray-500 outline-none text-white'
-              >Friends</button>
+            InstaLite&nbsp;
             </div>
-            */
         </div>
             <div className='font-bold text-3xl'>ChatBot</div>
+            <div className='h-[40rem] w-[30rem] bg-slate-100 p-3'>
+                <div className='h-[90%] overflow-scroll'>
+                    <div className='space-y-2'>
+                        {messages.map(msg => {
+                            return (
+                                <MessageComponent sender={msg.sender} message={msg.message} />
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className='w-full flex space-x-2'>
+                    <input className='w-full outline-none border-none px-3 py-1 rounded-md'
+                        placeholder='Ask something!'
+                        onChange={e => setInput(e.target.value)}
+                        value={input}
+                        onKeyDown={e => {
+                            if (e.key === 'Enter') {
+                                sendMessage();
+                                setInput('');
+                            }
+                        }} />
+                    <button className='outline-none px-3 py-1 rounded-md text-bold bg-indigo-600 text-white'
+                        onClick={() => {
+                            sendMessage();
+                        }}>Send</button>
+                </div>
+            </div>
         </div>
     )
 }
